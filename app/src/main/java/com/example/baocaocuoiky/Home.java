e8aeaf36eb239681f960d1828a3af8911d493009
Linh1605 <hoanglinh16052001@gmail.com>
2021-12-17 15:27:21 +0700

    Linh update

---
+++ app/src/main/java/com/example/baocaocuoiky/Home.java
@@ -0,0 +1,33 @@
+package com.example.baocaocuoiky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+    Button btn_dn,btn_dky;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btn_dn = findViewById(R.id.btn_dn);
+        btn_dky = findViewById(R.id.btn_dky);
+       btn_dky.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent  intent = new Intent(Home.this, DangKy.class);
+               startActivity(intent);
+           }
+       });
+       btn_dn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(Home.this, DangNhap.class);
+               startActivity(intent);
+           }
+       });
+    }
+}
--- app/src/main/java/com/example/baocaocuoiky/khaobaothanhcong.java
+++ app/src/main/java/com/example/baocaocuoiky/khaobaothanhcong.java
@@ -2,23 +2,13 @@ package com.example.baocaocuoiky;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class DangNhap extends AppCompatActivity {
-
+public class khaobaothanhcong extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dangnhap);
-
-
-
-
-
-
+        setContentView(R.layout.activity_khaobaothanhcong);
     }
 }
