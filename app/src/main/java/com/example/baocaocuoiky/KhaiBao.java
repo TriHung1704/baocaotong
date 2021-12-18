a3bf7a71ea3a79b59b85b8487f6f066e4021d044
TriHung1704 <trihungduong.1704@gmail.com>
2021-12-18 08:26:21 +0700

    hungcoomit

--- app/src/main/java/com/example/baocaocuoiky/KhaiBao.java
+++ app/src/main/java/com/example/baocaocuoiky/KhaiBao.java
@@ -3,6 +3,8 @@ package com.example.baocaocuoiky;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +12,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class TrangChu extends AppCompatActivity {
+public class KhaiBao extends AppCompatActivity {
+    Button bt_tieptuc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.trangchu);
+        setContentView(R.layout.thongtincanhan);
         BottomNavigationView bt  = findViewById(R.id.botton_navigation);
 
+        bt_tieptuc = findViewById(R.id.tieptuc);
 
+        bt_tieptuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(KhaiBao.this, KhaoSatbenh.class);
+                startActivity(intent);
+            }
+        });
         bt.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch (item.getItemId()){
                     case R.id.navigation_home:
-                        Intent trangchu = new Intent(TrangChu.this, TrangChu.class);
+                        Intent trangchu = new Intent(KhaiBao.this, TrangChu.class);
                         startActivity(trangchu);
                         break;
                     case R.id.navigation_khaibao:
-                        Intent giohang = new Intent(TrangChu.this,KhaiBao.class);
+                        Intent giohang = new Intent(KhaiBao.this,KhaiBao.class);
                         startActivity(giohang);
                         break;
 
                     case R.id.navigation_user:
-                        Intent thongbao = new Intent(TrangChu.this,ThongTinCaNhan.class);
+                        Intent thongbao = new Intent(KhaiBao.this,ThongTinCaNhan.class);
                         startActivity(thongbao);
                         break;
 
--- app/src/main/java/com/example/baocaocuoiky/MainActivity.java
+++ app/src/main/java/com/example/baocaocuoiky/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.khaibao);
+        setContentView(R.layout.activity_khaibaothanhcong);
     }
 }
