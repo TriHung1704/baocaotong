9b6ca66bbfd09b5d48063967590f8e6ae1daef16
TriHung1704 <trihungduong.1704@gmail.com>
2021-12-18 10:39:25 +0700

    hungcomit

--- app/src/main/java/com/example/baocaocuoiky/khaibaothanhcong.java
+++ app/src/main/java/com/example/baocaocuoiky/khaibaothanhcong.java
@@ -6,12 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.baocaocuoiky.databinding.KhaibaoBinding;
+
 public class khaibaothanhcong extends Activity {
+   Button bt_quayve;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_khaibaothanhcong);
+        bt_quayve = findViewById(R.id.trove);
 
+        bt_quayve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(khaibaothanhcong.this,TrangChu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
